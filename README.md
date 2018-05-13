30ea2d2e484480c51301fa6b6cd1aaaf9ba4b928
Jeff Shillitto <jeff@shillitto.com>
2018-05-13 22:57:58 +1000

    Add titles demo

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Shotstack PHP Examples
 
+- **text.php** -
+    Create a HELLO WORLD video title against black background with a soundtrack.
+
 - **images.php** -
     Takes an array of Instagram image URLs and creates a video with a soundtrack 
     and simple zoom in effect.
-    
-- **text.php** -
-    Create a HELLO WORLD video title against black background with a soundtrack.
+
+- **titles.php** -
+    Create a video to demo titles using the available presets with soundtrack and fade in/out transition.
     
 - **status.php** -
     Shows the status of a render task and the output video URL. Run this after running one of the render examples.
---
+++ examples/titles.php
@@ -0,0 +1,104 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Shotstack\Api\RenderApi;
+use Shotstack\ApiClient;
+use Shotstack\Configuration;
+use Shotstack\Model\Edit;
+use Shotstack\Model\Output;
+use Shotstack\Model\Soundtrack;
+use Shotstack\Model\Timeline;
+use Shotstack\Model\TitleClip;
+use Shotstack\Model\TitleClipOptions;
+use Shotstack\Model\Track;
+use Shotstack\Model\Transition;
+
+class TitlesDemo
+{
+    protected $styles = [
+        'minimal',
+        'blockbuster',
+        'vogue',
+        'sketchy',
+        'skinny',
+    ];
+
+    public function render()
+    {
+        $config = new Configuration();
+        $config
+            ->setHost(getenv('SHOTSTACK_HOST'))
+            ->setApiKey('x-api-key', getenv('SHOTSTACK_KEY'));
+
+        $client = new ApiClient($config);
+
+        $soundtrack = new Soundtrack();
+        $soundtrack
+            ->setEffect("fadeInOut")
+            ->setSrc("https://s3-ap-southeast-2.amazonaws.com/shotstack-public/dreams.mp3");
+
+        $clips = [];
+        $start = 0;
+        $length = 4;
+
+        foreach ($this->styles as $index => $style) {
+            $options = new TitleClipOptions();
+            $options
+                ->setEffect($style);
+
+            $transition = new Transition();
+            $transition
+                ->setIn('fadeIn')
+                ->setOut('fadeOut');
+
+            $clip = new TitleClip();
+            $clip
+                ->setType('title')
+                ->setSrc($style)
+                ->setIn(0)
+                ->setOut($length)
+                ->setStart($start)
+                ->setTransition($transition)
+                ->setOptions($options);
+
+            $start += $length;
+
+            $clips[] = $clip;
+        }
+
+        $track1 = new Track();
+        $track1
+            ->setClips($clips);
+
+        $timeline = new Timeline();
+        $timeline
+            ->setBackground("#000000")
+            ->setSoundtrack($soundtrack)
+            ->setTracks([$track1]);
+
+        $output = new Output();
+        $output
+            ->setFormat('mp4')
+            ->setResolution('sd');
+
+        $edit = new Edit();
+        $edit
+            ->setTimeline($timeline)
+            ->setOutput($output);
+
+        $render = new RenderApi($client);
+
+        try {
+            $response = $render->postRender($edit)->getResponse();
+        } catch (Exception $e) {
+            die('Request failed: ' . $e->getMessage());
+        }
+
+        echo $response->getMessage() . "\n";
+        echo ">> Now check the progress of your render by running:\n";
+        echo ">> php examples/status.php " . $response->getId() . "\n";
+    }
+}
+
+$editor = new TitlesDemo();
+$editor->render();
