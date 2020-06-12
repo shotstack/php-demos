ec0395f7e5ae0c1605f4cc88265996f6483ba76e
Jeff Shillitto <jeff@shillitto.com>
2020-06-12 12:22:53 +1000

    Add first luma matte transition

---
+++ examples/luma.php
@@ -0,0 +1,100 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Shotstack\Client\Api\EndpointsApi;
+use Shotstack\Client\Configuration;
+use Shotstack\Client\Model\Edit;
+use Shotstack\Client\Model\Output;
+use Shotstack\Client\Model\Soundtrack;
+use Shotstack\Client\Model\Timeline;
+use Shotstack\Client\Model\Track;
+use Shotstack\Client\Model\Clip;
+use Shotstack\Client\Model\LumaAsset;
+use Shotstack\Client\Model\VideoAsset;
+
+class LumaDemo
+{
+    protected $apiKey;
+    protected $apiUrl = 'https://api.shotstack.io/stage';
+
+    public function __construct()
+    {
+        if (empty(getenv('SHOTSTACK_KEY'))) {
+            die("API Key is required. Set using: export SHOTSTACK_KEY=your_key_here\n");
+        }
+
+        if (!empty(getenv('SHOTSTACK_HOST'))) {
+            $this->apiUrl = getenv('SHOTSTACK_HOST');
+        }
+
+        $this->apiKey = getenv('SHOTSTACK_KEY');
+    }
+
+    public function render()
+    {
+        $config = Configuration::getDefaultConfiguration()
+            ->setHost($this->apiUrl)
+            ->setApiKey('x-api-key', $this->apiKey);
+
+        $client = new EndpointsApi(null, $config);
+
+        $soundtrack = new Soundtrack();
+        $soundtrack
+            ->setEffect("fadeOut")
+            ->setSrc("https://shotstack-assets.s3-ap-southeast-2.amazonaws.com/music/unminus/palmtrees.mp3");
+
+        // First luma matte - top layer (track1)
+        $lumaAsset1 = new LumaAsset();
+        $lumaAsset1
+            ->setSrc('https://shotstack-assets.s3-ap-southeast-2.amazonaws.com/examples/luma-mattes/paint-left.mp4');
+
+        $lumaClip1 = new Clip();
+        $lumaClip1
+            ->setAsset($lumaAsset1)
+            ->setStart(3.88)
+            ->setLength(1.12);
+
+        $videoAsset1 = new VideoAsset();
+        $videoAsset1
+            ->setSrc('https://shotstack-assets.s3-ap-southeast-2.amazonaws.com/footage/table-mountain.mp4');
+
+        $videoClip1 = new Clip();
+        $videoClip1
+            ->setAsset($videoAsset1)
+            ->setStart(0)
+            ->setLength(5);
+
+        $track1 = new Track();
+        $track1
+            ->setClips([$lumaClip1, $videoClip1]);
+
+        $timeline = new Timeline();
+        $timeline
+            ->setBackground("#000000")
+            ->setSoundtrack($soundtrack)
+            ->setTracks([$track1]);
+
+        $output = new Output();
+        $output
+            ->setFormat('mp4')
+            ->setResolution('sd');
+
+        $edit = new Edit();
+        $edit
+            ->setTimeline($timeline)
+            ->setOutput($output);
+
+        try {
+            $response = $client->postRender($edit)->getResponse();
+        } catch (Exception $e) {
+            die('Request failed: ' . $e->getMessage());
+        }
+
+        echo $response->getMessage() . "\n";
+        echo ">> Now check the progress of your render by running:\n";
+        echo ">> php examples/status.php " . $response->getId() . "\n";
+    }
+}
+
+$editor = new LumaDemo();
+$editor->render();
